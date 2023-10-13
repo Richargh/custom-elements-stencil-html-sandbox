b88cb6fe6fae55419ede3dc55e48100cc6b4e844
Richard Gross <richard.gross@maibornwolff.de>
2023-10-13 08:57:50 +0200

    F: add db-ui fonts + icons + images + css as well

--- dist.sh
+++ dist.sh
@@ -1,7 +1,14 @@
 #!/usr/bin/env bash
 
 # DB-UI
-cp -r node_modules/@db-ui/elements/dist/db-ui-elements/ public/db-ui/dist
+mkdir -p public/db-ui/dist/core
+mkdir -p public/db-ui/dist/elements
+cp -r node_modules/@db-ui/elements/dist/db-ui-elements/ public/db-ui/dist/elements
+cp -r node_modules/@db-ui/core/dist/fonts/ fonts
+cp -r node_modules/@db-ui/core/dist/icons/ icons
+cp -r node_modules/@db-ui/core/dist/images/ images
+cp -r node_modules/@db-ui/core/dist/js/ js
+cp -r node_modules/@db-ui/core/dist/css/ css
 
 # GoatUI
 cp -r node_modules/@goatui/components/dist/goatui/ public/goatui/dist
